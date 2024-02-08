76a4d7694ad2111c4535ac5d38155c2eb7d23daf
liza <elizavetakulik2@gmail.com>
2024-02-08 14:09:26 +0100

    commit

---
+++ js/hw7.js
@@ -0,0 +1,21 @@
+// let a = 0;
+// while (a < 3) {
+//     a++;
+// }
+
+
+// let result = '';
+// let i = 0;
+
+// do {
+//     i = i + 1;
+//     result = result + i;
+// } while (i < 5);
+
+
+// let str = '';
+
+// for (let i = 0; i < 9; i++) {
+//     str = str + i;
+// }
+
