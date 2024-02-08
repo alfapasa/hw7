fd0edd8bb1fcc76d7f499c0ca376b6b9754f4891
liza <elizavetakulik2@gmail.com>
2024-02-08 15:17:38 +0100

    commit

--- js/hw7_4.js
+++ js/hw7_4.js
@@ -1,9 +1,9 @@
-// Знайти суму всіх цілих чисел від 1 до 15.
+// // Знайти суму всіх цілих чисел від 1 до 15.
 
-let result = 0;
+// let result = 0;
 
-for (let a = 0; a < 15; a++) {
-    result = result + a;
-}
+// for (let a = 0; a < 15; a++) {
+//     result = result + a;
+// }
 
-document.write(result);
+// document.write(result);
---
+++ js/hw7_5.js
@@ -0,0 +1,9 @@
+// //Знайти добуток усіх цілих чисел від 15 до 35.
+
+// let result = 1;
+
+// for (let a = 15; a < 35; a++) {
+//     result = result * a;
+// }
+
+// document.write(result);
