33fb8ed39910ec8426a5d674c642da68c5392dad
liza <elizavetakulik2@gmail.com>
2024-02-08 14:45:10 +0100

    commit

--- js/hw7_2.js
+++ js/hw7_2.js
@@ -1,10 +1,10 @@
-// Вивести квадрати чисел від 10 до 20.
+// // Вивести квадрати чисел від 10 до 20.
 
-let i = 10;
-let result = 0;
+// let i = 10;
+// let result = 0;
 
-while (i <= 20) {
-    result = i * i;
-    i++;
-    document.write(result + ', ')
-}
+// while (i <= 20) {
+//     result = i * i;
+//     i++;
+//     document.write(result + ', ')
+// }
