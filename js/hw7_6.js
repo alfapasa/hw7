285b0dba2b86e392334bb5e6321a30e9e0d63c48
liza <elizavetakulik2@gmail.com>
2024-02-11 23:44:18 +0100

    commit

---
+++ js/hw7_6.js
@@ -0,0 +1,11 @@
+//Знайти середнє арифметичне всіх цілих чисел від 1 до 500.
+
+let result = 0;
+
+for (let i = 1; i <= 500; i++) {
+    result = (result + i);
+}
+
+let average = result / 500;
+
+document.write(average);
